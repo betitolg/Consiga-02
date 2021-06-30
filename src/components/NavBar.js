77a8015586f49dc16e483b349cb5b2620a76d590
Daniel Lopez <betitolg@hotmail.com>
2021-06-30 14:18:24 -0500

    Estilos listos

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,12 @@ import react from "react";
 export default function NavBar() {
   return (
     <div className="tx_container">
-      <h1>Soy el Nav Bar</h1>
+      <ul>
+  <li><a class="active" href="#home">Home</a></li>
+  <li><a href="#news">News</a></li>
+  <li><a href="#contact">Contact</a></li>
+  <li><a href="#about">About</a></li>
+</ul>
     </div>
   );
 }
