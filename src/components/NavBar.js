a39ac2921072adf65a130f7c5783aea59462498d
Daniel Lopez <betitolg@hotmail.com>
2021-06-29 20:31:59 -0500

    Primer Commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,9 @@
+import react from "react";
+
+export default function NavBar() {
+  return (
+    <div className="tx_container">
+      <h1>Soy el Nav Bar</h1>
+    </div>
+  );
+}
---
+++ src/components/Text.js
@@ -0,0 +1,9 @@
+import react from "react";
+
+export default function Text() {
+  return (
+    <div className="tx_container">
+      <h1>Soy el componente Text</h1>
+    </div>
+  );
+}
